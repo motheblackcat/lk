8affc5c3f7d37f676c805b868cc0ee3b6629de2d
motheblackcat <motheblackcat@free.fr>
2017-10-21 15:35:37 +0200

    Update project

--- Camera/CameraControl.js
+++ Camera/CameraControl.js
@@ -3,6 +3,5 @@ function Update() {
     if (GameObject.FindWithTag("Player") != null){
         var player = GameObject.FindWithTag("Player");
         transform.position.x = player.transform.position.x;
-        //transform.position.y = player.transform.position.y + 3;
     }
 }
