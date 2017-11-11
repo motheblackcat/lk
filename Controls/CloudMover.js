ae439724f73d515b2fc5397a45e617892e1ca067
motheblackcat <motheblackcat@free.fr>
2017-11-11 20:33:15 +0100

    Added cloud moving

--- Controls/CloudMover.js
+++
@@ -1,5 +0,0 @@
-﻿var speed = 1.0;
-
-function Update() {
-    transform.Translate(Vector2.left * speed * Time.deltaTime);
-}
--- Controls/Scene1Manager.cs
+++ Controls/Scene1Manager.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Scene1Manager : MonoBehaviour {
+	GameObject[] clouds;
+	float[] speeds = new float[3];
+
 	void Start() {
 		Cursor.visible = false;
-		GameObject.Find("Sky").transform.parent = GameObject.Find("MainCamera").transform;		
+		GameObject.Find("Sky").transform.parent = GameObject.Find("MainCamera").transform;
+		clouds = GameObject.FindGameObjectsWithTag("Cloud");
+		for (int i = 0; i < clouds.Length; i++) {
+			speeds[i] = Random.value;
+		}
 	}
 
-	void Update () {
+	void FixedUpdate () {
+		for (int i = 0; i < clouds.Length; i++) {
+			clouds[i].transform.Translate(Vector2.left * (speeds[i] / 3) * Time.deltaTime);
+			Debug.Log(clouds[i] + " " + speeds[i] / 3);
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D other) {
