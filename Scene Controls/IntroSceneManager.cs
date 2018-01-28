bda5fd5e722ae9ef1d4343d24b33501feae3aada
motheblackcat <motheblackcat@free.fr>
2018-01-28 16:52:30 +0100

    Added fadeout to intro

---
+++ Scene Controls/IntroSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroSceneManager : MonoBehaviour {
+	Image fadeImage;
+	public float speed = 1.0f;
+	
+	void Start () {
+		fadeImage = GameObject.Find("Fade Image").GetComponent<Image>();
+		fadeImage.enabled = true;
+	}
+	
+	void FixedUpdate () {
+		FadeOut();
+	}
+
+	void FadeOut() {
+		Color clearColor = Color.clear;
+		fadeImage.color = Color.Lerp(fadeImage.color, clearColor, Time.time * Time.deltaTime * speed);
+	}
+}
