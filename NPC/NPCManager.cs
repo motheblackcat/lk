f195a93f8afd34ff316dfd31347ab768d8625132
motheblackcat <motheblackcat@free.fr>
2018-01-29 00:29:11 +0100

    WIP: NPC intro manager

--- NPC/NPCManager.cs
+++ NPC/NPCManager.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NPCManager : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-		
-	}
+	bool introDone = false;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate() {
+		if (Time.time > GameObject.Find("MainCamera").GetComponent<IntroSceneManager>().startTimer) {
+			introDone = true;
+		}
+	}
+
+	void ArrowManager() {
+		if (this.name == "Door") {
+			GetComponentInChildren<SpriteRenderer>().enabled = true;
+			if (Input.GetAxis("Vertical") > 0) {
+				SceneManager.LoadScene("Scene_1_RoadtoForest");
+			}
+		} else if (introDone) {
+			GetComponentsInChildren<SpriteRenderer>()[1].enabled = true;
+		}
+	}
+
+	void BartenderManager(GameObject player) {
+		if (this.name == "Bartender") {
+			if (player.transform.position.x > transform.position.x) {
+				GetComponent<SpriteRenderer>().flipX = true;
+			} else {
+				GetComponent<SpriteRenderer>().flipX = false;				
+			}
+
+			GetComponent<Animator>().SetBool("watch", true);
+			
+			if (Input.GetAxis("Vertical") > 0) {
+				GetComponent<Animator>().SetBool("talk", true);
+			}
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
-			GameObject.Find("ArrowUp").GetComponent<SpriteRenderer>().enabled = true;
-			if (this.name == "Door" && Input.GetAxis("Vertical") > 0) {
-				SceneManager.LoadScene("Scene_1_RoadtoForest");
-			}
+			ArrowManager();
+			BartenderManager(other.gameObject);
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
-			GameObject.Find("ArrowUp").GetComponent<SpriteRenderer>().enabled = false;
+			if (this.name == "Door") {
+				GetComponentInChildren<SpriteRenderer>().enabled = false;
+			} else {
+				GetComponentsInChildren<SpriteRenderer>()[1].enabled = false;
+			}
+
+			if (this.name == "Bartender") {
+				GetComponent<Animator>().SetBool("watch", false);
+				GetComponent<Animator>().SetBool("talk", false);
+			}
 		}
 	}
 }
