ed99559deeb0b97370844cd31710e4009f857bb4
Mo <morad.elhamdouni@gmail.com>
2018-09-30 16:15:15 +0200

    Update player animation script

--- Player/PlayerAnimation.cs
+++ Player/PlayerAnimation.cs
@@ -7,53 +7,69 @@ public class PlayerAnimation : MonoBehaviour {
 	PlayerControl playerControl;
 	PlayerHealth playerHealth;
 	PlayerAttack playerAttack;
+	public GameObject sword;
 
 	void Start () {
 		animator = GetComponent<Animator>();
 		playerControl = GetComponent<PlayerControl>();
 		playerHealth = GetComponent<PlayerHealth>();
 		playerAttack = GetComponent<PlayerAttack>();
+		sword = GameObject.Find("Sword");
 	}
-	
-	void Update () {
-		// Had conditions in the case the component is null
-		if (playerControl) {
-			if (playerControl.canMove) {
-				if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0) {
-					animator.SetBool("run", true);
-				} else {
-					animator.SetBool("run", false);
-				}
-			}
 
-			if (playerControl.isGrounded) {
-				animator.SetBool("air", false);
-			} else {
-				animator.SetBool("air", true);
-			}
-		}
-		
-		if (playerAttack) {
-			if (playerAttack.isAttacking) {
-				animator.SetBool("attack", true);
-			}
-			else {
-				animator.SetBool("attack", false);
-			}
+	void Update() {
+		PlayerAnimate();
+		SwordPosition();
+	}
+
+	void PlayerAnimate() {
+		if (playerControl.canMove) {
+			animator.SetBool("run", Input.GetAxis("Horizontal") != 0 ? true : false);
+			sword.GetComponent<Animator>().Play(Input.GetAxis("Horizontal") != 0 ? "Sword_Run" : "Sword_Idle");
 		}
 
-		if (playerHealth) {
-			if (playerHealth.isHurt) {
-				animator.SetBool("hurt", true);
-			}
-			else {
-				animator.SetBool("hurt", false);
-			}
+		animator.SetBool("air", !playerControl.isGrounded);
+		// TODO: Try to refactor this IF & check if sword atk jump do not mess landing anim
+		if (!playerControl.isGrounded) {
+			sword.GetComponent<Animator>().Play("Sword_Jump");
+		}
 
-			// TOFIX: If isHurt is not used the ghost appears before the death animation
-			if (playerHealth.isDead && !playerHealth.isHurt) {
-				GetComponent<Animator>().SetTrigger("die");
-			}
+		// TODO: Can all be done with conditional operators
+		// if (playerAttack) {
+		// 	if (playerAttack.isAttacking) {
+		// 		animator.SetBool("attack", true);
+		// 	}
+		// 	else {
+		// 		animator.SetBool("attack", false);
+		// 	}
+		// }
+
+		// if (playerHealth) {
+		// 	if (playerHealth.isHurt) {
+		// 		animator.SetBool("hurt", true);
+		// 	}
+		// 	else {
+		// 		animator.SetBool("hurt", false);
+		// 	}
+
+		// 	// TOFIX: If isHurt is not used the ghost appears before the death animation
+		// 	if (playerHealth.isDead && !playerHealth.isHurt) {
+		// 		GetComponent<Animator>().SetTrigger("die");
+		// 	}
+		// }
+	}
+
+	// Check if this method can be refactored
+	void SwordPosition() {
+		if (GetComponent<SpriteRenderer>().flipX) {
+			sword.GetComponent<SpriteRenderer>().flipX = true;
+			sword.transform.localPosition = new Vector2(-0.74f, sword.transform.localPosition.y);
+			sword.GetComponent<BoxCollider2D>().offset = new Vector2(-0.25f, 0.45f);
+		} else {
+			sword.GetComponent<SpriteRenderer>().flipX = false;
+			sword.transform.localPosition = new Vector2(0.74f, sword.transform.localPosition.y);
+			sword.GetComponent<BoxCollider2D>().offset = new Vector2(0.25f, 0.45f);
 		}
 	}
+
 }
