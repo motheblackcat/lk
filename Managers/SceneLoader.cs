652228c51b7362065d44aff056f1ab42890a9b86
Mo <morad.elhamdouni@gmail.com>
2019-03-17 14:59:41 +0100

    Using scene index rather than long names

--- Managers/SceneLoader.cs
+++ Managers/SceneLoader.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
-    public string sceneIndex;
+    public int sceneIndex;
 
 	void OnTriggerExit2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
