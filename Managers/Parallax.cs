a60121a8c2e83df5e944ceb7dd275aceda359d5e
Mo <morad.elhamdouni@gmail.com>
2019-07-19 21:25:09 +0200

    Fixed parallax for a simpler version

--- Managers/Parallax.cs
+++ Managers/Parallax.cs
@@ -2,29 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour {
-	public Transform[] backgrounds;
-	float[] paraScales;
-	Transform cam;
-	Vector3 prevCamPos;
-	public float smoothing = 1f;
+public class Parallax : MonoBehaviour
+{
+    Transform camPos;
+    public Vector3 camCoord;
+    public Transform bgPos;
+    public Vector3 bgCoord;
+    public float smoothing = 1f;
 
-	void Start() {
-		cam = Camera.main.transform;
-		prevCamPos = cam.position;
-		paraScales = new float[backgrounds.Length];
-		for (int i = 0; i < backgrounds.Length; i++) {
-			paraScales[i] = backgrounds[i].position.z * -1;
-		}
-	}
-
-	void Update() {
-		for (int i = 0; i < backgrounds.Length; i++) {
-			float parallax = (prevCamPos.x - cam.position.x) * paraScales[i];
-			float bgTargetPosX = backgrounds[i].position.x + parallax;
-			Vector3 bgTargetPos = new Vector3 (bgTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
-			backgrounds[i].position = Vector3.Lerp (backgrounds[i].position, bgTargetPos, smoothing * Time.deltaTime);
-		}
-		prevCamPos = cam.position;		
-	}
+    void FixedUpdate()
+    {
+        camPos = Camera.main.transform;
+        camCoord = camPos.position;
+        bgCoord = bgPos.position;
+        bgPos.position = new Vector3((camPos.position.x / smoothing) - 10, bgPos.position.y, bgPos.position.z);
+    }
 }
