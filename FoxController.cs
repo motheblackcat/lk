49d048a4d7e7273efab08df948e48a304f4f83d4
Mo <morad.elhamdouni@gmail.com>
2019-07-20 18:40:43 +0200

    Fixed pause menu

---
+++ FoxController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float rbValues;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rbValues = rb.velocity.x;
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            rb.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0));
+        }
+    }
+}
--- Menus/PauseMenu.cs
+++ Menus/PauseMenu.cs
@@ -3,66 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PauseMenu : MonoBehaviour {
-	public bool paused = false;
-	Image joyCon;
-	Image keyCon;
+public class PauseMenu : MonoBehaviour
+{
+    public bool paused = false;
+    Image joyCon;
+    Image keyCon;
     GameObject pauseUI;
-	GameObject player;
-	PlayerControl playerControl;
-	PlayerAnimation playerAnimation;
-	PlayerAttack playerAttack;
-	// PlayerAudio	playerAudio;
+    GameObject player;
+    PlayerControl playerControl;
+    PlayerAnimation playerAnimation;
+    PlayerAttack playerAttack;
 
-	void Start() {
-		player = GameObject.Find("Player");
-		playerControl = player.GetComponent<PlayerControl>();
-		playerAnimation = player.GetComponent<PlayerAnimation>();
-		playerAttack = player.GetComponent<PlayerAttack>();
-		// playerAudio = player.GetComponent<PlayerAudio>();
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerControl = player.GetComponent<PlayerControl>();
+        playerAnimation = player.GetComponent<PlayerAnimation>();
+        playerAttack = player.GetComponent<PlayerAttack>();
         pauseUI = GameObject.Find("PauseUI");
         pauseUI.GetComponent<AudioSource>().ignoreListenerPause = true;
         pauseUI.GetComponent<Canvas>().enabled = true;
         joyCon = GameObject.Find("JoyCon Text").GetComponent<Image>();
         keyCon = GameObject.Find("KeyCon Text").GetComponent<Image>();
-	}
+        joyCon.enabled = false;
+        keyCon.enabled = true;
+    }
 
-	void Update() {
-		GamepadDetection();
-		PauseDetection();
-	}
+    void Update()
+    {
+        GamepadDetection();
+        PauseDetection();
+    }
 
-	void GamepadDetection() {
-		// Gamepad detection is clunky
-		foreach (string gamePad in Input.GetJoystickNames()) {
-			joyCon.enabled = gamePad == "" ? false : true;
-			keyCon.enabled = gamePad == "" ? true : false;
-		}
-	}
+    void GamepadDetection()
+    {
+        foreach (string gamePad in Input.GetJoystickNames())
+        {
+            joyCon.enabled = gamePad == "" ? false : true;
+            keyCon.enabled = gamePad == "" ? true : false;
+            Debug.Log(Input.GetJoystickNames() + " " + gamePad);
+        }
+    }
 
-	void PauseDetection() {
-		if (Input.GetButtonDown("Start")) { paused = !paused; }
+    void PauseDetection()
+    {
+        if (Input.GetButtonDown("Start")) { paused = !paused; }
         if (paused) { Pause(); } else { Resume(); }
-	}
+    }
 
-	void Pause() {
-		Time.timeScale = 0;
-		AudioListener.pause = true;
-		pauseUI.SetActive(true);
-		playerControl.enabled = false;
-		playerAnimation.enabled = false;
-		playerAttack.enabled = false;
-		// playerAudio.enabled = false;
-		if (Input.GetButtonDown("Select")) { Application.Quit(); }
-	}
+    void Pause()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        pauseUI.SetActive(true);
+        playerControl.enabled = false;
+        playerAnimation.enabled = false;
+        playerAttack.enabled = false;
+        if (Input.GetButtonDown("Select")) { Application.Quit(); }
+    }
 
-	void Resume() {
-		Time.timeScale = 1;
-		AudioListener.pause = false;
-		pauseUI.SetActive(false);
-		playerControl.enabled = true;
-		playerAnimation.enabled = true;
-		playerAttack.enabled = true;
-		// playerAudio.enabled = true;
-	}
+    void Resume()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseUI.SetActive(false);
+        playerControl.enabled = true;
+        playerAnimation.enabled = true;
+        playerAttack.enabled = true;
+    }
 }
