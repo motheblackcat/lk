9fe396f5a48d123643e22ad2ec61d08bdc49821f
Mo <morad.elhamdouni@gmail.com>
2019-07-20 20:25:48 +0200

    Update readme and tweaking bob event

--- Managers/AxeEventHandler.cs
+++ Managers/AxeEventHandler.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AxeEventHandler : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.tag == "Player") {
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && GameObject.Find("DialogBox").GetComponent<DialogManager>().inDialog)
+        {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        }
+    }
+
+    // NEED A MORE GENERAL WAY TO HANDLE NPC EVENTS
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            GetComponent<Animator>().enabled = false;
+
+        }
+        if (other.gameObject.tag == "Player")
+        {
             Destroy(gameObject);
         }
     }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # LK
+
 Unity 2D heroic-fantasy adventure game demo.
 
 My first attempt at making a 2D game demo with the Unity engine, my goal is to acheive a small but satisfying action oriented first level before expanding toward a more complexe metroidvania game style.
@@ -8,3 +9,9 @@ Feel free to contribute in any way you wish!
 <img src="https://motheblackcat.github.io/assets/img/game.gif">
 
 You can test the current build here: https://motheblackcat.github.io
+
+KNOWN ISSUES:
+
+- Attack system is clunky the enemy feedback seems delayed
+- Enemy death animation plays after a long delay
+- Need a generic handler for NPC Events
