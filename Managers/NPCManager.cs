f059cb1be1efc3dce1e7ec02041dc69bae5d116b
Mo <morad.elhamdouni@gmail.com>
2019-07-20 17:05:59 +0200

    Remove up arrow during dialogs

--- Managers/NPCManager.cs
+++ Managers/NPCManager.cs
@@ -1,33 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class NPCManager : MonoBehaviour {
-    void OnTriggerStay2D(Collider2D other) {
-		if (other.gameObject.tag == "Player" && Camera.main.GetComponent<IntroSceneManager>().introDone) {
-			GameObject.Find(this.name + "/ArrowUp").GetComponent<SpriteRenderer>().enabled = true;
-			if (GetComponent<Animator>()) {
-				GetComponent<Animator>().SetBool("watch", true);
-				GetComponent<Animator>().SetBool("talk", GameObject.Find("DialogBox").GetComponent<Image>().enabled);
-			}
-			if (tag == "NPC") {
-				GetComponent<SpriteRenderer>().flipX = other.gameObject.transform.position.x > transform.position.x;
-			}
-			if (tag == "Door" && Input.GetAxis("Vertical") > 0) {
-				SceneManager.LoadScene(1);
-			}
-		}
-	}
+public class NPCManager : MonoBehaviour
+{
+    bool dialogOpen;
+    SpriteRenderer dialogArrow;
 
-	void OnTriggerExit2D(Collider2D other) {
-		if (other.gameObject.tag == "Player") {
-			if (GetComponent<Animator>()) {
-				GetComponent<Animator>().SetBool("watch", false);
-				GetComponent<Animator>().SetBool("talk", false);
-			}
-			GameObject.Find(this.name + "/ArrowUp").GetComponent<SpriteRenderer>().enabled = false;
-		}
-	}
+    void Update()
+    {
+        dialogOpen = GameObject.Find("DialogBox").GetComponent<Image>().enabled;
+        dialogArrow = GameObject.Find(this.name + "/ArrowUp").GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && Camera.main.GetComponent<IntroSceneManager>().introDone)
+        {
+            dialogArrow.enabled = !dialogOpen;
+            if (GetComponent<Animator>())
+            {
+                GetComponent<Animator>().SetBool("watch", true);
+                GetComponent<Animator>().SetBool("talk", dialogOpen);
+            }
+            if (tag == "NPC")
+            {
+                GetComponent<SpriteRenderer>().flipX = other.gameObject.transform.position.x > transform.position.x;
+            }
+            if (tag == "Door" && Input.GetAxis("Vertical") > 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            dialogArrow.enabled = false;
+            Debug.Log(dialogArrow.enabled);
+            if (GetComponent<Animator>())
+            {
+                GetComponent<Animator>().SetBool("watch", false);
+                GetComponent<Animator>().SetBool("talk", false);
+            }
+        }
+    }
 }
