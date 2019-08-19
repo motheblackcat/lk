4eaf6d39362679581e98f7f3243e0763e48af609
Morad El Hamdouni <morad.elhamdouni@gmail.com>
2019-08-19 14:20:10 +0200

    Update sWeaponControl with sounds array idea

--- SWeapons/SWeaponsControl.cs
+++ SWeapons/SWeaponsControl.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SWeaponsControl : MonoBehaviour {
+    public AudioClip sWeaponSound = null;
     public int throwForceX = 0;
     public int throwForceY = 0;
     public int weaponDamage = 0;
-    public AudioClip sWeaponSound = null;
     public float throwTimerCd = 0;
+    AudioClip[] sounds = sWeaponSound[];
 
     private void Awake() {
         CheckWeaponType();
+        sounds = GameObject.Find('sWeaponsSounds').GetComponent<Sounds>().sounds;
     }
 
     void CheckWeaponType() {
@@ -20,13 +22,20 @@ public class SWeaponsControl : MonoBehaviour {
                 throwForceX = 8;
                 throwForceY = 20;
                 weaponDamage = 5;
-                // How to get soundclip?
+                // How to get soundclip (ressources.load or array of sounds)?
                 // sWeaponSound = null;
+                // sWeaponSound = GetSound();
                 throwTimerCd = 0.5f;
                 break;
             default:
                 break;
         }
+    }
+    
+    AudioClip GetSound() {
+        // Check string operation in C#
+        string sWeaponName = name.toLowerCase();
+        sounds.find(sWeaponName + "ThrowSound");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
