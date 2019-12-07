1ceee64d5997b2962392d0a15e8c82443d87c9a8
Mo <morad.elhamdouni@gmail.com>
2019-12-07 22:59:24 +0100

    DialogManager Refacto done

--- Managers/DialogManager.cs
+++ Managers/DialogManager.cs
@@ -3,17 +3,17 @@ using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour {
     public GameObject npc;
-    GameObject player;
     Canvas dialogUI;
     PlayerControl playerControl;
     IntroSceneManager introSceneManager;
+    GlobalManager globalManager;
     public bool inDialog = false;
 
     void Start() {
-        player = GameObject.Find("Player");
         dialogUI = GameObject.Find("DialogUI").GetComponent<Canvas>();
-        playerControl = player.GetComponent<PlayerControl>();
+        playerControl = GameObject.Find("Player").GetComponent<PlayerControl>();
         introSceneManager = GameObject.Find("GameManager").GetComponent<IntroSceneManager>();
+        globalManager = GameObject.Find("GameManager").GetComponent<GlobalManager>();
     }
 
     void Update() {
@@ -21,24 +21,29 @@ public class DialogManager : MonoBehaviour {
         inDialog = dialogUI.enabled;
         bool autoStartDialog = npc ? npc.GetComponent<NpcAnimation>().autoStart : false;
         bool introDone = introSceneManager ? introSceneManager.introDone : true;
+        bool isGamepad = globalManager.isGamepad;
 
         if (npc && introDone) {
             if (autoStartDialog) {
-                getDialog(npc);
+                getDialog();
                 autoStartDialog = false;
             }
             if (Input.GetButtonDown("Jump")) {
-                getDialog(npc);
+                getDialog();
             }
-
             if (inDialog && Input.GetButtonDown("Jump")) {
                 GameObject.Find("DialogText").GetComponent<Text>().text = "";
                 dialogUI.enabled = false;
             }
+            GameObject.Find(npc.name + "/ButtonA").GetComponent<SpriteRenderer>().enabled = !inDialog && isGamepad;
+            GameObject.Find(npc.name + "/SpaceBar").GetComponent<SpriteRenderer>().enabled = !inDialog && !isGamepad;
+        } else {
+            GameObject[] buttons = GameObject.FindGameObjectsWithTag("NPCButton");
+            foreach (GameObject button in buttons)button.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 
-    void getDialog(GameObject npc) {
+    void getDialog() {
         string path = "Text/" + npc.name + "Dialog";
         TextAsset text = Resources.Load<TextAsset>(path);
         GameObject.Find("DialogText").GetComponent<Text>().text = text.text;
