fb96f90c8769e6e141230ab52c520784848fe3d3
Mo <morad.elhamdouni@gmail.com>
2019-12-20 06:25:38 +0100

    Fix pause sound + method to optimize

--- Menus/PauseMenuManager.cs
+++ Menus/PauseMenuManager.cs
@@ -5,6 +5,7 @@ public class PauseMenuManager : MonoBehaviour {
     Image gamepadUI;
     Image keyboardUI;
     Canvas pauseUI;
+    AudioSource audioSource;
     GlobalManager globalManager;
     public bool paused = false;
 
@@ -13,16 +14,22 @@ public class PauseMenuManager : MonoBehaviour {
         keyboardUI = GameObject.Find("KeyboardUI").GetComponent<Image>();
         globalManager = GameObject.Find("GameManager").GetComponent<GlobalManager>();
         pauseUI = GameObject.Find("PauseUI").GetComponent<Canvas>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.ignoreListenerPause = true;
     }
 
     void Update() {
-        if (Input.GetButtonDown("Start"))paused = !paused;
+        if (Input.GetButtonDown("Start"))Pause();
         if (paused && Input.GetButtonDown("Select"))Application.Quit();
         gamepadUI.enabled = globalManager.isGamepad;
         keyboardUI.enabled = !globalManager.isGamepad;
+    }
+
+    void Pause() {
+        paused = !paused;
         Time.timeScale = paused ? 0 : 1;
         AudioListener.pause = paused;
-        GetComponent<AudioSource>().enabled = paused;
+        audioSource.enabled = paused;
         pauseUI.enabled = paused;
     }
 }
