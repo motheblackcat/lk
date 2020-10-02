750d5ecd579db7711c14e985722e7c39f594a215
motheblackcat <morad.elhamdouni@gmail.com>
2020-10-02 04:13:28 +0200

    Remove unused code

--- Enemies/EnemyMove.cs
+++ Enemies/EnemyMove.cs
@@ -6,11 +6,9 @@ public class EnemyMove : StateMachineBehaviour {
     Transform player;
     public float moveSpeed = 5f;
     public float moveRange = 20f;
-    public float attackRange = 0f;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         /** TODOL: Set enemy speed, move & attack ranges according to type */
-        /** TODOL: Keep in mind evolution for ranged enemies */
         rigidbody = animator.GetComponent<Rigidbody2D>();
         spriteRenderer = animator.GetComponent<SpriteRenderer>();
         player = GameObject.FindWithTag("Player").transform;
@@ -23,11 +21,8 @@ public class EnemyMove : StateMachineBehaviour {
             Vector2 newPos = Vector2.MoveTowards(rigidbody.position, target, moveSpeed * Time.fixedDeltaTime);
             rigidbody.MovePosition(newPos);
             spriteRenderer.flipX = player.position.x > rigidbody.transform.position.x;
-            if (Vector2.Distance(player.position, rigidbody.position) <= attackRange) animator.SetTrigger("atk");
         }
     }
 
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        animator.ResetTrigger("atk");
-    }
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) { }
 }
